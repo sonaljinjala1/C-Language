ae00876c3e9a0dd6e1869d100ed7fd0e0b105b06
Sonal Jinjala <139161113+sonaljinjala1@users.noreply.github.com>
2024-05-06 11:22:16 +0530

    Add files via upload

---
+++ PR.7-Gathering/q-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "q-1.c"
+
+void main()
+{
+	printf("Press 1 for +..\n");
+	printf("Press 2 for -..\n");
+	printf("Press 3 for *..\n");
+	printf("Press 4 for /..\n");
+	printf("Press 5 for %..\n");
+	printf("Press 0 for exit..\n");
+	
+	choiceInput();
+}
